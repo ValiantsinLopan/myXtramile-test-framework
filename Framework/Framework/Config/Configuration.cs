e05041fa76ddc5d0cbec46c131dd08c69eb58281
Valiantsin Lopan <valiantsin_lopan@live.com>
2017-04-08 13:17:27 +0300

    base architecture of framework

---
+++ Framework/Framework/Config/Configuration.cs
@@ -0,0 +1,68 @@
+﻿using Framework.Resources;
+using System;
+using System.Collections;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium.Remote;
+
+namespace Framework.Config
+{
+    public static class Configuration
+    {
+        private const string ConfigFilePath = "Framework/app.config";
+
+        private static System.Configuration.Configuration _config;
+        private static System.Configuration.Configuration Config => _config ?? (_config = LoadConfig());
+
+        /// <summary>
+        /// Loads configuration from file
+        /// </summary>
+        /// <returns>loaded and parsed conf file</returns>
+        private static System.Configuration.Configuration LoadConfig()
+        {
+            var configMap = new ExeConfigurationFileMap
+            {
+                ExeConfigFilename = $"{SolutionPath}/{ConfigFilePath}"
+            };
+            return ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+        }
+
+        /// <summary>
+        /// Get full solution path form currently called assembly
+        /// </summary>
+        /// <returns>full solution path</returns>
+        public static string SolutionPath => Uri.UnescapeDataString(new DirectoryInfo(new Uri(Assembly.GetCallingAssembly().GetName().CodeBase).AbsolutePath).Parent.Parent.Parent.Parent.FullName);
+
+        public static string CurrentBrowserName => Config.AppSettings.Settings["browser.current"].Value;
+
+        public static string ChromeBinPath => $"{SolutionPath}/{DriversPaths.ChromeDriver}";
+        public static string FirefoxBinPath => $"{SolutionPath}/{DriversPaths.FirefoxDriver}";
+        public static string EdgeBinPath => $"{SolutionPath}/{DriversPaths.EdgeDriver}";
+        public static string IeBinPath => $"{SolutionPath}/{DriversPaths.IEDriver}";
+
+        /// <summary>
+        /// Loads all specified capabilities from config file
+        /// </summary>
+        /// <returns>loaded capabilities</returns>
+        public static DesiredCapabilities LoadBrowserStackCapabilities()
+        {
+            var capabilities = new DesiredCapabilities();
+
+            var set = BrowserStack.ResourceManager.GetResourceSet(CultureInfo.InvariantCulture, true, true);
+            foreach (DictionaryEntry variable in set)
+            {
+                try
+                {
+                    var capabilityKey = (string)variable.Value;
+                    var capabilityValue = Config.AppSettings.Settings[capabilityKey].Value;
+                    capabilities.SetCapability(capabilityKey, capabilityValue);
+                }
+                catch (NullReferenceException) { }
+            }
+
+            return capabilities;
+        }
+    }
+}
---
+++ Framework/Framework/WebDriver/DriverFactory.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Framework.Config;
+using Framework.Resources;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+namespace Framework.WebDriver
+{
+    public static class DriverFactory
+    {
+        private const string Chrome = "chrome";
+        private const string Firefox = "firefox";
+        private const string Ie = "ie";
+        private const string Edge = "edge";
+        private const string Remote = "remote";
+
+        private static readonly ConcurrentDictionary<string, IWebDriver> DriversDictionary = new ConcurrentDictionary<string, IWebDriver>();
+        private static string CurrentTest => TestContext.CurrentContext.Test.ClassName;
+
+        public static void CleanupDriver() { Driver.Quit(); }
+
+        public static IWebDriver Driver
+        {
+            get
+            {
+                IWebDriver driver;
+                if (DriversDictionary.TryGetValue(CurrentTest, out driver)) return driver;
+                InitDriver();
+
+                return DriversDictionary[CurrentTest];
+            }
+        }
+
+        public static void InitDriver()
+        {
+            DriversDictionary.TryAdd(CurrentTest, GetDriverInstance());
+            Driver.Manage().Window.Maximize();
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        }
+
+        /// <summary>
+        /// Creates driver specified by current test name
+        /// </summary>
+        /// <exception cref="InvalidOperationException">if driver with specified name wasn't found</exception>
+        /// <returns></returns>
+        private static IWebDriver GetDriverInstance()
+        {
+            switch (Configuration.CurrentBrowserName)
+            {
+                case Firefox:
+                    {
+                        return GetFirefoxDriver();
+                    }
+                case Ie:
+                    {
+                        return GetIeDriver();
+                    }
+                case Edge:
+                    {
+                        return GetEdgeDriver();
+                    }
+                case Remote:
+                    {
+                        return GetRemoteDriver();
+                    }
+                case Chrome:
+                default:
+                    {
+                        return GetChromeDriver();
+                    }
+            }
+        }
+
+        private static IWebDriver GetChromeDriver()
+        {
+            return new ChromeDriver(Configuration.ChromeBinPath);
+        }
+
+        private static IWebDriver GetFirefoxDriver()
+        {
+            var service = FirefoxDriverService.CreateDefaultService(Configuration.FirefoxBinPath);
+            return new FirefoxDriver(service);
+        }
+
+        private static IWebDriver GetEdgeDriver()
+        {
+            var service = EdgeDriverService.CreateDefaultService(Configuration.EdgeBinPath);
+            return new EdgeDriver(service);
+        }
+
+        private static IWebDriver GetIeDriver()
+        {
+            var service = InternetExplorerDriverService.CreateDefaultService(Configuration.IeBinPath);
+            return new InternetExplorerDriver(service);
+        }
+
+        private static IWebDriver GetRemoteDriver()
+        {
+            var capabilities = Configuration.LoadBrowserStackCapabilities();
+            return new RemoteWebDriver(new Uri(BrowserStack.URL), capabilities);
+        }
+    }
+}
