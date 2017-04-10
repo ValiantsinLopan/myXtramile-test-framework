8d69de5f4c1cb5ca1f4cc4ce4a8612362d419f2e
Valiantsin Lopan <valiantsin_lopan@live.com>
2017-04-10 21:17:05 +0300

    deserialization add to data provider

---
+++ Framework/Framework/BusinessObjects/User.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Framework.BusinessObjects
+{
+    public class User
+    {
+        [XmlElement("email")]
+        public string Email { get; set; }
+
+        [XmlElement("password")]
+        public string Password { get; set; }
+
+        public User()
+        {
+
+        }
+        public User(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
---
+++ Framework/NUnitTests/DataSources/UsersDataSources.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using Framework.BusinessObjects;
+using Framework.Config;
+using Framework.Resources;
+
+
+namespace NUnitTests.DataSources
+{
+    public class UsersDataSources
+    {
+        private static string UsersPath => $"{Configuration.SolutionPath}/{TestDataPaths.UsersPath}";
+
+        public static IEnumerable TestCaseWithUsers()
+        {
+           var data = AllUsers.DeserialiseUsers(UsersPath);
+            foreach(var user in data.Users)
+            {
+                yield return new TestCaseData(user.Email, user.Password);
+            }
+        }
+    }
+}
