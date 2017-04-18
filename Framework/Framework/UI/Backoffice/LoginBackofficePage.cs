c1c1834e29bade8e113884a2b49b91178091db90
Valiantsin <valiantsin_lopan@live.com>
2017-04-19 00:48:46 +0300

    Backoffice page objects created

---
+++ Framework/Framework/UI/Backoffice/LoginBackofficePage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using Framework.WebDriver;
+
+namespace Framework.UI.Backoffice
+{
+    public class LoginBackofficePage
+    {
+
+        private const string Url = "http://backoffice.xtramile.tech/";
+        private IWebElement LoginInput => DriverFactory.Driver.FindElement(By.XPath("//input[@id='email']"));
+        private IWebElement PasswordInput => DriverFactory.Driver.FindElement(By.XPath("//input[@id='password']"));
+        private IWebElement SignInButton => DriverFactory.Driver.FindElement(By.XPath("//button[@class='btn btn-primary btn-block']"));
+
+        public void NavigateHere()
+        {
+            DriverFactory.Driver.Navigate().GoToUrl(Url);
+        }
+        public void LoginBackoffice(string login, string password)
+        {
+            LoginInput.SendKeys(login);
+            PasswordInput.SendKeys(password);
+            SignInButton.Click();
+        }
+
+    }
+}
---
+++ Framework/Framework/UI/Backoffice/UsersPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using Framework.WebDriver;
+
+namespace Framework.UI.Backoffice
+{
+    public class UsersPage
+    {
+        private IWebElement FilterInput => DriverFactory.Driver.FindElement(By.XPath("input[@aria-controls='DataTables_Table_1']"));
+
+        public void SearchByEmail(string email)
+        {
+            FilterInput.SendKeys(email);
+        }
+    }
+}
