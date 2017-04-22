a8f343a4f670320d995f3af8ee8e9e2b840707f8
Valiantsin <valiantsin_lopan@live.com>
2017-04-22 20:05:37 +0300

    Account pages in progress

---
+++ Framework/Framework/UI/Pages/MainPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using Framework.WebDriver;
+
+namespace Framework.UI.Pages
+{
+    public class MainPage
+    {
+        private IWebElement UserDropdown => DriverFactory.Driver.FindElement(By.XPath("//li[@class='dropdown dropdown-user']/a"));
+        private IWebElement AccountSettings => DriverFactory.Driver.FindElement(By.XPath("//ul[@class='dropdown-menu dropdown-menu-right']//a[@href='/myaccount/company']"));
+    }
+}
---
+++ Framework/Framework/UI/Pages/PaymentInformationForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using Framework.WebDriver;
+
+namespace Framework.UI.Pages
+{
+    public class PaymentInformationForm
+    {
+    }
+}
